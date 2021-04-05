e5b98361e0d5e809d04a7248b1b04e6616fa63f3
Murilo Souza <moslite@hotmail.com>
2021-04-05 06:13:44 -0300

    OrderStatus enum

--- src/main/java/com/myspring/order/entities/Order.java
+++ src/main/java/com/myspring/order/entities/Order.java
@@ -1,6 +1,7 @@
 package com.myspring.order.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.myspring.order.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -20,6 +21,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -27,9 +30,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +55,16 @@ public class Order implements Serializable {
 
     public User getClient() {
         return client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public void setClient(User client) {
