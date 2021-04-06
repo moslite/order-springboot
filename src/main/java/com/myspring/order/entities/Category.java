a15bcc2f35cc085dba205cca2f3c3dd60389939a
Murilo Souza <moslite@hotmail.com>
2021-04-06 05:59:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/myspring/order/entities/Category.java
+++ src/main/java/com/myspring/order/entities/Category.java
@@ -1,5 +1,7 @@
 package com.myspring.order.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -15,8 +17,11 @@ public class Category implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-    @Transient
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
