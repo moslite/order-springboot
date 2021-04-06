1fa41538f18ae2d3a1baf0d3a9b39b211ecf45c2
Murilo Souza <moslite@hotmail.com>
2021-04-06 06:43:26 -0300

    Bug fix

--- src/main/java/com/myspring/order/repositories/OrderItemRepository.java
+++ src/main/java/com/myspring/order/repositories/OrderItemRepository.java
@@ -1,9 +1,10 @@
 package com.myspring.order.repositories;
 
 import com.myspring.order.entities.OrderItem;
+import com.myspring.order.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 
 }
